d4a59620fca9c21d55bfa86881e5ad95128b1665
Sandeepkandula2004 <136213405+Sandeepkandula2004@users.noreply.github.com>
2024-03-03 12:00:06 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,42 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+hands = HandDetector(detectionCon=0.8, maxHands=2)
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    success, img = cap.read()
+    
+    allhands, img = hands.findHands(img=img, draw=True, flipType=True)
+    
+    if allhands:
+
+        for myhand in allhands:
+
+            count = hands.fingersUp(myHand=myhand)
+            if myhand['type'] == 'Left':
+                if count.count(1) == 5:
+                    keyboard.release(Key.left)
+                else:
+                    keyboard.press(Key.left)
+
+            if myhand['type'] == 'Right':
+                if count.count(1) == 0:
+                    keyboard.press(Key.right)
+                else:
+                    keyboard.release(Key.right)
+                    
+         
+
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
